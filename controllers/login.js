473b62ff7bdc0dc61b07f16909978a655638ae5b
Miraz Ahmed <miraz37@gmail.com>
2021-02-18 12:40:01 -0500

    Create Web Server with Express
    Closes #2
    Initiated web server.

---
+++ controllers/login.js
@@ -0,0 +1,3 @@
+export const login = (req,res) =>{
+    res.send('From Log in page!');
+}
---
+++ routes/login.js
@@ -0,0 +1,9 @@
+import express from 'express';
+
+import { login } from '../controllers/login.js'
+
+const router = express.Router();
+
+router.get('/',login );
+
+export default router;
